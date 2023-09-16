725f7b66444cab8710ca070cc1d61b5eb8bbf2a5
tehcneko <chsqwyx@gmail.com>
2023-09-16 12:05:52 +0800

    Remove buymeacoffee

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -54,19 +54,6 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', size: '16x16', href: '/icon/favicon-16x16.png' }],
     ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-BFBNXGKZQW' }],
     ['script', {}, ["window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-BFBNXGKZQW');"]],
-    ['script', { 
-      'data-name': 'BMC-Widget',
-       'data-cfasync': false,
-       src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
-       'data-id': 'nekogram',
-       'data-description': 'Support me on Buy me a coffee!',
-       'data-message': 'Support me on Buy me a coffee!',
-       'data-color': '#5F7FFF',
-       'data-position': 'Right',
-       'data-x_margin': '18',
-       'data-y_margin': '18',
-       }
-    ],
   ],
   locales: {
     '/': {
